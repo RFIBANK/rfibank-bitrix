6c9ccfd8658d1401f77a7b92f1097ab95447bd4a
romanof76 <romanof76@gmail.com>
2015-12-24 13:05:12 +0300

    разделены по кодировкам

---
+++ rficb.payment_utf8/lang/ru/options.php
@@ -0,0 +1,9 @@
+﻿<?
+$MESS["RFICB.PAYMENT_OPTIONS_KEY"] = "Ключ";
+$MESS["RFICB.PAYMENT_OPTIONS_KEY_DESC"] = "Генерируется при создании кнопки на сайте ЗАО \"РФИ БАНК\"";
+$MESS["RFICB.PAYMENT_OPTIONS_SECRET_KEY"] = "Секретный ключ";
+$MESS["RFICB.PAYMENT_OPTIONS_SECRET_KEY_DESC"] = "Устанавливается при создании сервиса в системе ЗАО \"РФИ БАНК\"";
+$MESS["RFICB.PAYMENT_OPTIONS_SCRIPT_URL"] = "URL скрипта обработчика на Вашем сайте";
+$MESS["RFICB.PAYMENT_OPTIONS_SCRIPT_URL_DESC"] = "Укажите данный адрес в настройках сервиса в системе ЗАО \"РФИ БАНК\"";
+$MESS["RFICB.PAYMENT_OPTIONS_ERROR"] = "Не заполнено обязательное поле \"#FIELD#\"";
+?>
